585861d08ee5ba225fe0b12833922c9eae3850c2
flame <76920703+flamebousteur@users.noreply.github.com>
2022-02-03 13:39:37 +0100

    add LZW encode / decode

--- v1/script.js
+++ v1/script.js
@@ -196,6 +196,58 @@ const clog = {
 		document.getElementById("clog").innerHTML = ""
 	}
 }
+
+// https://gist.github.com/revolunet/843889
+// LZW-compress a string
+function lzw_encode(s) {
+	let dict = {};
+	let data = (s + "").split("");
+	let out = [];
+	let currChar;
+	let phrase = data[0];
+	let code = 256;
+	for (let i=1; i<data.length; i++) {
+		currChar=data[i];
+		if(dict[phrase + currChar] != null){
+			phrase += currChar;
+		}else{
+			out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+			dict[phrase + currChar] = code;
+			code++;
+			phrase=currChar;
+		}
+	}
+	out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+	for(let i=0; i<out.length; i++){
+		out[i] = String.fromCharCode(out[i]);
+	}
+	return out.join("");
+}
+
+// Decompress an LZW-encoded string
+function lzw_decode(s) {
+	let dict = {};
+	let data = (s + "").split("");
+	let currChar = data[0];
+	let oldPhrase = currChar;
+	let out = [currChar];
+	let code = 256;
+	let phrase;
+	for(let i=1; i<data.length; i++){
+		var currCode = data[i].charCodeAt(0);
+		if(currCode < 256){
+			phrase = data[i];
+		}else{
+			phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
+		}
+		out.push(phrase);
+		currChar = phrase.charAt(0);
+		dict[code] = oldPhrase + currChar;
+		code++;
+		oldPhrase = phrase;
+	}
+	return out.join("");
+}
 /*end lib*/
 
 const dconfig = {
